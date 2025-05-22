4bdeb890a50deb494e5828629020f0d663f6303b
LucasDellaMaestra <lucas.casluu33@gmail.com>
2025-05-22 00:28:11 +0000

    ejercicio flask

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+import numpy as np
+import pickle
 
-# your code here
+app = Flask(__name__)
+
+# Cargar modelo y transformador
+model = pickle.load(open("xgboost_model.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_input = np.array(features).reshape(1, -1)
+    transformed_input = transformer.transform(final_input)
+    prediction = model.predict(transformed_input)
+    return render_template("index.html", prediction_text=f"Predicción: {'Diabético' if prediction[0]==1 else 'No diabético'}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
